1f27e055e37282bce65368e239d776027fc9c77e
danitw <twz3xc21@gmail.com>
2020-07-28 22:59:21 -0300

    CSS É MT CHATO AAAAAAAAAAAAA

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Home</title>
+	<title>Casa das Kpopers</title>
 	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
 </head>
@@ -11,9 +11,11 @@
 
 	<main>
 
-		<div id="top-bar">
+		<header>
+			<div class="search-box">
 
-		</div>
+			</div>
+		</header>
 
 		<div id="column-1">
 			1 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
