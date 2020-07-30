a98043c2d84fe257881cd70f3047ca60e8234683
danitw <twz3xc21@gmail.com>
2020-07-30 04:41:34 -0300

    update, algumas adições no lado do header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Casa das Kpopers</title>
 	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
+	<script src="https://kit.fontawesome.com/60e76d6bf1.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 <body>
 
@@ -23,12 +24,18 @@
 				</div>
 
 				<div class="top-right">
-					<a class="write-post" href="#"></a>
+					<!-- <a class="write-post" href="#">Write a post <span class="far fa-keyboard"></span> </a> -->
 
-					<a class="notifications" href="#"></a>
+					<a class="write-post" href="#">
+						<button type="button" class="btn btn-primary">Write a post</button>
+					</a>
+
+					<a class="notifications " href="#">
+						<span class="far fa-bell"></span>
+					</a>
 
 					<div class="menu">
-
+						<a class="fas fa-bars"></a>
 					</div>
 
 				</div>
@@ -62,6 +69,8 @@
 
 
 
-
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
