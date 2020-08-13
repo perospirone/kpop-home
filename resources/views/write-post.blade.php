843cccffe1ddda8485caaacc4ed7b88d37417d9e
danitw <twz3xc21@gmail.com>
2020-08-13 18:31:38 -0300

    mexendo nas rotas, adicionando o local de escrever os post(começando)

---
+++ resources/views/write-post.blade.php
@@ -0,0 +1,47 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Casa das Kpopers</title>
+    <link rel="stylesheet" href="{{ asset('css/write-post.css') }}">
+    <script src="https://kit.fontawesome.com/60e76d6bf1.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/script.js') }}"> </script>
+  </head>
+  <body>
+    <header class="">
+      <div class="top-bar-container maxWidth divF">
+
+        <div class="flexDiv">
+          <!-- <div class="logo">
+            Casa das Kpopers
+            </div> -->
+
+            <div class="text-top">
+              <span>Write a new post</span>
+            </div>
+
+            <div class="edit-preview">
+              <button type="button" class="btn btn-light">Edit</button>
+              <button type="button" class="btn btn-light">Preview</button>
+            </div>
+        </div>
+
+        
+
+      </div>
+
+      <div class="close">
+        <a href="{{ URL::route('home') }}">
+          <i class="fas fa-times" width="24" height="24"></i>
+        </a>
+      </div>
+    </header>
+
+
+  </body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/new', function () {
+  return view('write-post');
+})->name('new');
