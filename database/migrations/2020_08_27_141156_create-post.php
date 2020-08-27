11d7be8db5ac9dbabeb651d299f61df4b127ace1
danitw <twz3xc21@gmail.com>
2020-08-27 11:57:51 -0300

    add migration

---
+++ database/migrations/2020_08_27_141156_create-post.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePost extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('publications', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->string('tag');
+        $table->string('path_cover_image');
+        $table->string('path_image');
+        $table->string('content');
+
+        $table->unsignedBigInteger('creator_id');
+
+        $table->foreign('creator_id')->references('id')->on('users')->onDelete('cascade');
+
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
