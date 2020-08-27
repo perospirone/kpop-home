ec6c91e91c9add9d4b8b99224a3ee7f1646df6e3
danitw <twz3xc21@gmail.com>
2020-08-27 09:13:00 -0300

    update form

--- resources/views/write-post.blade.php
+++ resources/views/write-post.blade.php
@@ -34,41 +34,42 @@
       </div>
     </header>
     <main class="post">
-      <div class="write">
-        <div class="text-padding">
-          <button type="button" class="cover-imagebtn">Add a cover image</button>
-          <div class="title">
-            <!-- <textarea type="text" placeholder="New post title here..." ></textarea> -->
-            <input placeholder="New post title here...">
+      <form action="@" method="POST">
+        <div class="write">
+          <div class="text-padding">
+            <button type="button" class="cover-imagebtn">Add a cover image</button>
+            <div class="title">
+              <!-- <textarea type="text" placeholder="New post title here..." ></textarea> -->
+              <input name="title" placeholder="New post title here...">
+            </div>
+
+            <div class="tags">
+              <input name="tags"  placeholder="Add up to 4 tags...">
+            </div>
           </div>
 
-          <div class="tags">
-            <input placeholder="Add up to 4 tags...">
+          <div class="text-padding">
+            <div class="upload-image">
+              <button class="btn">
+                <i class="fas fa-image"></i>
+                Upload image
+              </button>
+            </div>
+            <textarea class="write-content" name="content"  placeholder="Write your post content here..."></textarea>
           </div>
         </div>
 
-        <div class="text-padding">
-          <div class="upload-image">
-            <button class="btn">
-              <i class="fas fa-image"></i>
-              Upload image
-            </button>
-          </div>
-          <textarea class="write-content" placeholder="Write your post content here..."></textarea>
+        <div class="trick">
+          Use markdown pow, fica daora
         </div>
-      </div>
 
-      <div class="trick">
-        Use markdown pow, fica daora
-      </div>
+        <div class="footer">
+          <button type="submit" class="btn btn-primary publish-btn">Publish</button>
+          <button class="btn btn-light save-draft">Save draft</button>
+          <button class="btn btn-light revert-btn">Revert new changes</button>
+        </div>
 
-      <div class="footer">
-        <button class="btn btn-primary publish-btn">Publish</button>
-        <button class="btn btn-light save-draft">Save draft</button>
-        <button class="btn btn-light revert-btn">Revert new changes</button>
-
-
-      </div>
+      </form>
     </main>
 
   </body>
