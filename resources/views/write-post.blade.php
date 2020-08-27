c4c9e95033591228e2acdc5ff983d6292a402cfb
danitw <twz3xc21@gmail.com>
2020-08-27 11:57:22 -0300

    update form post

--- resources/views/write-post.blade.php
+++ resources/views/write-post.blade.php
@@ -34,10 +34,14 @@
       </div>
     </header>
     <main class="post">
-      <form action="@" method="POST">
+      <form action="{{ route('create.post')  }}" method="POST" enctype="multipart/form-data">
+        @csrf
         <div class="write">
           <div class="text-padding">
-            <button type="button" class="cover-imagebtn">Add a cover image</button>
+            <button type="button" class="cover-imagebtn">
+              Add a cover image
+              <input name="cover-image" type="file">
+            </button>
             <div class="title">
               <!-- <textarea type="text" placeholder="New post title here..." ></textarea> -->
               <input name="title" placeholder="New post title here...">
@@ -50,9 +54,10 @@
 
           <div class="text-padding">
             <div class="upload-image">
-              <button class="btn">
+              <button type="button" class="btn">
                 <i class="fas fa-image"></i>
-                Upload image
+                Upload image   
+                <input type="file" name="image">
               </button>
             </div>
             <textarea class="write-content" name="content"  placeholder="Write your post content here..."></textarea>
