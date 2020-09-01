2c2d5183094dcfeeb2bee3f3b6619a46b6d10d4e
danitw <twz3xc21@gmail.com>
2020-09-01 19:07:24 -0300

    retirando um comentario e corrigindo uma identação

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+  return view('index');
 });
 
 Auth::routes();
@@ -26,7 +26,5 @@ Route::get('/new', function () {
 })->name('new');
 
 Route::post('/posts/create', 'PostController@create')->name('create.post');
-
-// Route::get('/post/')
 
 Route::post('/posts/{id}', 'PostController@addLikes');
