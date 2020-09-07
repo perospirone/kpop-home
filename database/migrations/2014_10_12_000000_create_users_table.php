e507f46a0ec6beddf2f580108543e88617f863aa
danitw <twz3xc21@gmail.com>
2020-09-07 18:06:35 -0300

    adicionando os status na tabela users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('description')->nullable();
+            $table->string('status')->nullable();
             $table->string('path_profile_image')->nullable();
             $table->string('twitter_user')->nullable();
             $table->rememberToken();
