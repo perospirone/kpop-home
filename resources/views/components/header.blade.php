662bd41b48473fc843e3d7612a9b4f96d281bc2a
danitw <twz3xc21@gmail.com>
2020-09-20 12:05:20 -0300

    identando direito o component header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,32 +1,31 @@
+<header class="">
+  <div class="top-bar-container maxWidth">
 
-    <header class="">
-      <div class="top-bar-container maxWidth">
-
-        <div class="flexDiv">
-          <div class="logo">
-            <a href="{{ URL::to('/') }}">Kpop Home</a>
-          </div>
-
-          <div class="search-box">
-            <form action="#" method="get">
-              <input type="text" name="" value="" placeholder="Pesquisar">
-            </form>
-          </div>
-        </div>
-
-        <div class="top-right">
-          <a href="{{ URL::route('new') }}" class="btn btn-primary write-post">
-            Write a post
-          </a>
-          <a class="iconButton" href="#">
-            <i class="far fa-bell"></i>
-          </a>
-          <div class="menu-profile">
-            <a href="{{ URL::to('/profile/' . auth()->user()->username ) }}">
-              <img src="{{ asset('storage/' . $user->path_profile_image ) }}"></img>
-            </a>
-          </div>
-
-        </div>
+    <div class="flexDiv">
+      <div class="logo">
+        <a href="{{ URL::to('/') }}">Kpop Home</a>
       </div>
-    </header>
+
+      <div class="search-box">
+        <form action="#" method="get">
+          <input type="text" name="" value="" placeholder="Pesquisar">
+        </form>
+      </div>
+    </div>
+
+    <div class="top-right">
+      <a href="{{ URL::route('new') }}" class="btn btn-primary write-post">
+        Write a post
+      </a>
+      <a class="iconButton" href="#">
+        <i class="far fa-bell"></i>
+      </a>
+      <div class="menu-profile">
+        <a href="{{ URL::to('/profile/' . auth()->user()->username ) }}">
+          <img src="{{ asset('storage/' . $user->path_profile_image ) }}"></img>
+        </a>
+      </div>
+
+    </div>
+  </div>
+</header>
