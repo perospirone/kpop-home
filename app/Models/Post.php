adf0285a1d9f9f408a6672af08e0a9821bdb96aa
danitw <twz3xc21@gmail.com>
2020-10-10 23:35:37 -0300

    atualizando dependencias e configurando coisas referentes ao cloudinary

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use CloudinaryLabs\CloudinaryLaravel\MediaAlly;
 
 class Post extends Model {
+  use MediaAlly;
+
 	protected $table = 'publications';
 	protected $fillable = ['title', 'tags', 'path_cover_image', 'path_image', 'content', 'creator_id', 'num_likes', 'num_comments', 'name_author', 'username_author'];
 
